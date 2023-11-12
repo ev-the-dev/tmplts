ab7f2454dbb98041d0907d7bb6395104cb02d013
Evan <ehutch7@gmail.com>
2023-11-12 03:00:02 +0000

    Playing around with an eslint config

--- templates/eslintconfig.templates.go
+++ templates/eslintconfig.templates.go
@@ -1,3 +1,41 @@
 package templates
 
-var eslintConfigTemplate = ``
+var eslintConfigTemplate = `
+import globals from "globals"
+import js from "@eslint/js"
+// import tslint from "@typescript-eslint"
+import tslintPlugin from "@typescript-eslint/eslint-plugin"
+import tslintParser from "@typescript-eslint/parser"
+
+export default [
+  js.configs.recommended,
+  // tslint.eslint-plugin.configs.recommended, // throws error on new config file type due to internal "extends"
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tslintParser,
+      parserOptions: {
+        ecmaFeatures: {
+          modules: true,
+          project: "./tsconfig.json"
+        }
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tslintPlugin,
+    },
+    rules: {
+      "no-await-in-loop": "error",
+      "no-constructor-return": "error",
+      "no-duplicate-imports": "error",
+      "camelcase": "error",
+    }
+  }
+]
+`
