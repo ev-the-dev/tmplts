44388d39e56fd0899c75ad3aa9733c74c4ea6f99
Evan <ehutch7@gmail.com>
2023-11-14 02:30:47 +0000

    Adding more rules to ESLint template -- onto TS rules next

--- templates/eslintconfig.templates.go
+++ templates/eslintconfig.templates.go
@@ -31,10 +31,43 @@ export default [
       "@typescript-eslint": tslintPlugin,
     },
     rules: {
+      /**
+       * OG ESLint Rules
+       */
       "no-await-in-loop": "error",
       "no-constructor-return": "error",
       "no-duplicate-imports": "error",
       "camelcase": "error",
+      "default-case-last": "error",
+      "default-param-last": "error",
+      "eqeqeq": "error",
+      "func-names": ["error", "as-needed"],
+      "max-depth": ["error", 2],
+      "max-nested-callbacks": ["error", 2],
+      "multiline-comment-style": "warn",
+      "no-empty-function": "warn",
+      "no-lone-blocks": "error",
+      "no-loop-func": "warn",
+      "no-nested-ternary": "error",
+      "no-param-reassign": "error",
+      "no-return-assign": "error",
+      "no-unneeded-ternary": "error",
+      "no-unused-expressions": "warn",
+      "no-useless-computed-key": "warn",
+      "no-useless-concat": "warn",
+      "no-useless-rename": "error",
+      "prefer-const": ["error", {
+        "destructuring": "any",
+        "ignoreReadBeforeAssign": false
+      }],
+      "sort-imports": "error",
+      "sort-keys": ["error", "asc", {
+        "natural": true
+      }],
+
+      /**
+       * TypeScript ESLint Rules
+       */
     }
   }
 ]
